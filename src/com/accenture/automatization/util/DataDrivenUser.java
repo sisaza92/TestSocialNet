292e65f720065d8102f1b6008b8cbb8d5f00ec84
sisaza92 <sisaza92@gmail.com>
2017-07-06 12:01:13 -0500

    Added credentials file and DataDrivenUser for manage it

--- src/com/accenture/automatization/util/DataDrivenUser.java
+++ src/com/accenture/automatization/util/DataDrivenUser.java
@@ -2,9 +2,9 @@ package com.accenture.automatization.util;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -17,45 +17,86 @@ public class DataDrivenUser {
 	public final String SHEET_NAME = "Sheet1";//"Test Credentials";
 	
 	public FileInputStream fileInputStream = null;
+	private FileOutputStream fileOutputStream = null;
 	public XSSFWorkbook workbook = null;
 	public XSSFSheet sheet = null;
+	XSSFRow row = null;
 
-	
+	private String xslFilePath;
+
+
+		
 	
 	public DataDrivenUser(String xslFilePath) throws IOException {
 		
-		fileInputStream = new FileInputStream(xslFilePath);
-        workbook = new XSSFWorkbook(fileInputStream);
-        fileInputStream.close();
+		this.xslFilePath = xslFilePath;
 		
 	}
 	
 	public Credential getCredential(int rowNum)
     {
+		Credential credential = null;
+		
 		XSSFRow row = null;
 		XSSFCell usernameCell = null;
 		XSSFCell passwordCell = null;
 		
-		Credential credential = null;
 		String username = "";
 		String password = "";
 		
-        sheet = workbook.getSheet(SHEET_NAME);
-        row = sheet.getRow(rowNum);
-        usernameCell = row.getCell(0);
-        passwordCell = row.getCell(1);
-        
-        username = usernameCell.getStringCellValue();
-        password = passwordCell.getStringCellValue();
-        
-        credential = new Credential(username, password);
+		try {
+			
+			fileInputStream = new FileInputStream(xslFilePath);
+			workbook = new XSSFWorkbook(fileInputStream);
+			
+	        sheet = workbook.getSheet(SHEET_NAME);
+	        row = sheet.getRow(rowNum);
+	        usernameCell = row.getCell(0);
+	        passwordCell = row.getCell(1);
+	        
+	        username = usernameCell.getStringCellValue();
+	        password = passwordCell.getStringCellValue();
+	        
+	        credential = new Credential(username, password);
+	        
+	        fileInputStream.close();
+	        
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         return credential;
         
     }
 	
-	public void setStatuss(String statuss){
+	public void setStatus(int rowNum, String statusMessage){
 		
+		XSSFRow row = null;
+		XSSFCell statusCell = null;
+		
+		try {
+			
+			fileOutputStream  = new FileOutputStream(xslFilePath);
+						
+	        sheet = workbook.getSheet(SHEET_NAME);
+	        row = sheet.getRow(rowNum);
+	        statusCell = row.createCell(2);	        
+	        statusCell.setCellValue(statusMessage);	        
+	        workbook.write(fileOutputStream);
+	        
+	        fileOutputStream.close();
+	        
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
